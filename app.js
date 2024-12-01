ee898c220e4438713c36247277cd6903eba72b42
m-euiyle <kyle.maderaje1@gmail.com>
2024-12-01 14:46:42 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -17,5 +17,15 @@ document.addEventListener('DOMContentLoaded', function() {
             sidebar.setAttribute('toggle', "open");
         }
 
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+        
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+
     });
 });
